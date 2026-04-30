282e24e5e95d17c8bb2c987576b1501cd116369e
wilson palma <wilsonpsouza8@gmail.com>
2026-04-30 15:15:14 -0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/crediario/crediario_api/CrediarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.crediario.crediario_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrediarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrediarioApiApplication.class, args);
+	}
+
+}
