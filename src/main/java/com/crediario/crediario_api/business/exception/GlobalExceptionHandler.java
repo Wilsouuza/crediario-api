2dd7007e9e48a38b1fb92355733b9ce11d31122b
wilson palma <wilsonpsouza8@gmail.com>
2026-05-01 18:21:03 -0300

    feat: add validation exception handler

--- src/main/java/com/crediario/crediario_api/business/exception/GlobalExceptionHandler.java
+++ src/main/java/com/crediario/crediario_api/business/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.crediario.crediario_api.business.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,11 +15,25 @@ public class GlobalExceptionHandler {
     public ErrorResponse handleBusinessException(BusinessException e){
         return new ErrorResponse(e.getMessage());
     }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     public ErrorResponse handleException(Exception e){
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ErrorResponse handleValidations(MethodArgumentNotValidException e){
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + " : " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        return new ErrorResponse(message);
     }
 
 }
